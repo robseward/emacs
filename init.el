ec1675063f88d47e36106557ea6c4329a7f1dc7c
Rob Seward <rseward@squareup.com>
2018-02-26 13:45:09 -0500

    enable ido mode and smex

--- init.el
+++ init.el
@@ -54,3 +54,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(ido-mode 1)
+(ido-everywhere 1)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; This is your old M-x.
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+

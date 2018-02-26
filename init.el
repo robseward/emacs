9bdeec289ca695b2fd169ec4633baed3a104892a
Rob Seward <rseward@squareup.com>
2018-02-26 16:42:02 -0500

    Increase default font size
    `M-x customize-face` RET `default` gives you the interface to set this

--- init.el
+++ init.el
@@ -55,7 +55,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :stipple nil :background "#000000" :foreground "#eaeaea" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 160 :width normal :foundry "nil" :family "Menlo")))))
 
 ;; ido
 (ido-mode 1)

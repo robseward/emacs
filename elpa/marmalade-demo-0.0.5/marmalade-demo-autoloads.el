08b23dc9ecad96a5e1f939b9f5b301f498a95b95
Rob Seward <rseward@squareup.com>
2018-02-26 10:31:18 -0500

    Add melpa, marmalade, and better-defaults

---
+++ elpa/marmalade-demo-0.0.5/marmalade-demo-autoloads.el
@@ -0,0 +1,22 @@
+;;; marmalade-demo-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "marmalade-demo" "marmalade-demo.el" (23188
+;;;;;;  9761 291129 862000))
+;;; Generated autoloads from marmalade-demo.el
+
+(autoload 'marmalade-demo "marmalade-demo" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; marmalade-demo-autoloads.el ends here

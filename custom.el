74096a006187b56edae0188ed8104b2c0083ae0c
Rob Seward <rseward@squareup.com>
2018-03-19 10:00:10 -0400

    iBuffer saved filter groups

--- custom.el
+++ custom.el
@@ -12,7 +12,16 @@
    (quote
     ("e7aa1c216e14ec250a367908e305dc7f2d549a4b3f87f4a0515ef5d3800fc2f5" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "bfdcbf0d33f3376a956707e746d10f3ef2d8d9caa1c214361c9c08f00a1c8409" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
  '(fci-rule-color "#373b41")
- '(ibuffer-saved-filter-groups (quote (("org" ("org" (used-mode . org-mode))))))
+ '(ibuffer-saved-filter-groups
+   (quote
+    (("org-and-markdown"
+      ("markdown"
+       (used-mode . markdown-mode))
+      ("org"
+       (used-mode . org-mode)))
+     ("org"
+      ("org"
+       (used-mode . org-mode))))))
  '(ibuffer-saved-filters
    (quote
     (("gnus"
